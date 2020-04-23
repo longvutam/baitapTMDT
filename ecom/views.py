d69f157dd37f3959af4087f2256861144521ffd4
phuong-08 <nvpcntt08@gmail.com>
2020-04-23 10:47:33 +0700

    finish file base and any more

--- ecom/views.py
+++ ecom/views.py
@@ -5,19 +5,19 @@ from django.views import View
 
 class homeView(View):
     def get(self, request):
-        return render(request, 'base.html')
+        return render(request, 'home.html')
 
 class productView(View):
     def get(self, request):
-        return render(request, 'product/product.html')
+        return render(request, 'product.html')
 
 class cartView(View):
     def get(self, request):
-        return render(request, 'cart/cart.html')
+        return render(request, 'cart.html')
 
 class checkoutView(View):
     def get(self, request):
-        return render(request, 'checkout/checkout.html')
+        return render(request, 'checkout.html')
 
 # def home_view(request):
 #     object_list = Page.objects.all().filter()
